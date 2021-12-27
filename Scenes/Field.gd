61dc38655956c56d0df6e856c99c78a096cb0680
ultcode <55034698+ultcode@users.noreply.github.com>
2021-12-27 22:55:51 +0100

    Add edge avoidance behaviour

--- Scenes/Field.gd
+++ Scenes/Field.gd
@@ -17,11 +17,54 @@ func _ready() -> void:
 
 
 func _on_Area2D_body_exited(body: Node) -> void:
+	if body.is_in_group("player"):
+		body.die()
+		return
 	body.queue_free()
 
 
 func _on_Area2D2_body_entered(body: Node) -> void:
-	var normal = Vector2(-10, 0)
+	var normal = Vector2(0, 1)
 	if (body.is_in_group("animals")):
-		body.velocity = body.velocity.reflect(normal)
-	pass # Replace with function body.
+		body.is_near_edge = true
+		body.edge_normal = normal
+
+
+func _on_Area2D2_body_exited(body: Node) -> void:
+	if (body.is_in_group("animals")):
+		body.is_near_edge = false
+
+
+func _on_Area2D3_body_entered(body: Node) -> void:
+	var normal = Vector2(1, 0)
+	if (body.is_in_group("animals")):
+		body.is_near_edge = true
+		body.edge_normal = normal
+
+func _on_Area2D3_body_exited(body: Node) -> void:
+	if (body.is_in_group("animals")):
+		body.is_near_edge = false
+
+
+func _on_Area2D4_body_entered(body: Node) -> void:
+	var normal = Vector2(0, -1)
+	if (body.is_in_group("animals")):
+		body.is_near_edge = true
+		body.edge_normal = normal
+
+
+func _on_Area2D4_body_exited(body: Node) -> void:
+	if (body.is_in_group("animals")):
+		body.is_near_edge = false
+
+
+func _on_Area2D5_body_entered(body: Node) -> void:
+	var normal = Vector2(-1, 0)
+	if (body.is_in_group("animals")):
+		body.is_near_edge = true
+		body.edge_normal = normal
+
+
+func _on_Area2D5_body_exited(body: Node) -> void:
+	if (body.is_in_group("animals")):
+		body.is_near_edge = false
