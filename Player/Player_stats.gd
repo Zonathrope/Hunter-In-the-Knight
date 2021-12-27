8aea8bcc52f04468a5a1b7096978faf01778150f
VektorDio <67824812+VektorDio@users.noreply.github.com>
2021-12-27 23:49:58 +0300

    ammo fix

--- Player/Player_stats.gd
+++ Player/Player_stats.gd
@@ -3,7 +3,7 @@ extends Node
 export var max_hearts: int = 3
 export var current_hearts: float = 3
 export var move_speed: int = 500
-export var ammo = 200
+export var ammo = 100
 
 export var attack_damage: float = 1.0
 
