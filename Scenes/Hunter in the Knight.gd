f267f21485a260209ff0d0e29112b29532c041be
VektorDio <67824812+VektorDio@users.noreply.github.com>
2021-12-28 02:02:25 +0300

    randomgen fix

--- Scenes/Hunter in the Knight.gd
+++ Scenes/Hunter in the Knight.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 var rabbits = 3
-var wolves = 3
-var deers = 9
+var wolves = 10
+var deers = 4
 
 var rabbitArray : Array
 var wolfArray : Array
@@ -10,22 +10,46 @@ var deerArray : Array
 
 func _ready():
 	while rabbits > 0:
-		var rabbit = preload("res://Animals/Rabbit/Rabbit.tscn").instance()
-		rabbit.global_position = Vector2(100, 100)
-		rabbitArray.append(rabbit) 
-		add_child(rabbit)
-		rabbits -= 1
+		rabbits *= 10
+		var rng  = RandomNumberGenerator.new()
+		rng.randomize()
+		var spawnPoint = Vector2(rng.randi_range(-600, 600), rng.randi_range(-600, 600))
+		while rabbits*10 > 0:
+			randomize()
+			var rabbit = preload("res://Animals/Rabbit/Rabbit.tscn").instance()
+			var magicNumber1 = randf()
+			var magicNumber2 = randf()
+			rabbit.global_position = Vector2(spawnPoint.x + 400*magicNumber1, spawnPoint.y + 400*magicNumber2)
+			rabbitArray.append(rabbit) 
+			add_child(rabbit)
+			rabbits -= 1
 	while wolves > 0:
-		var wolf = preload("res://Animals/Wolf/Wolf.tscn").instance()
-		wolf.global_position = Vector2(-100, 100)
-		wolfArray.append(wolf) 
-		add_child(wolf)
-		wolves -= 1
+		var rng  = RandomNumberGenerator.new()
+		rng.randomize()
+		var spawnPoint = Vector2(rng.randi_range(-600, 600), rng.randi_range(-600, 600))
+		while wolves > 0:
+			randomize()
+			var magicNumber1 = randf()
+			var magicNumber2 = randf()
+			var wolf = preload("res://Animals/Wolf/Wolf.tscn").instance()
+			wolf.global_position = Vector2(spawnPoint.x + 500*magicNumber1, spawnPoint.y + 500*magicNumber2)
+			wolfArray.append(wolf) 
+			add_child(wolf)
+			wolves -= 1
 	while deers > 0:
-		var deer = preload("res://Animals/Deer/Deer.tscn").instance()
-		deer.global_position = Vector2(100, -100)
-		deerArray.append(deer) 
-		add_child(deer)
+		var rng  = RandomNumberGenerator.new()
+		rng.randomize()
+		var spawnPoint = Vector2(rng.randi_range(-600, 600), rng.randi_range(-600, 600))
+		var deersAmount = rng.randi_range(2, 6)
+		while deersAmount > 0:
+			randomize()
+			var deer = preload("res://Animals/Deer/Deer.tscn").instance()
+			var magicNumber1 = randf()
+			var magicNumber2 = randf()
+			deer.global_position = Vector2(spawnPoint.x + 200*magicNumber1, spawnPoint.y + 200*magicNumber2)
+			deerArray.append(deer) 
+			add_child(deer)
+			deersAmount -= 1
 		deers -= 1
 
 func getRabbitsArray():
