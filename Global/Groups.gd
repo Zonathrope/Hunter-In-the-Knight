078ccac6fbf243b68dd31c00b6b75bb7dc921531
VektorDio <67824812+VektorDio@users.noreply.github.com>
2021-12-27 23:22:43 +0300

    sprite fix

--- Global/Groups.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-const Doors = 'doors'
-const Players = 'players'
-const Rooms = 'rooms'
-const Enemies = 'enemies'
-const BranchConnectionPoints = 'branch_connection_points'
-const Corridors = 'corridors'
